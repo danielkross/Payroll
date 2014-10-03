098095abd5575eb83b067a6889aa404e27d547a5
strzupak <strzupak@gmail.com>
2014-10-03 12:41:58 +0100

    adding new customer and new supplier to database functionality

--- tpokorasite/payroll/views.py
+++ tpokorasite/payroll/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.utils import timezone
 from payroll.models import Customer, Supplier
 
 # Create your views here.
@@ -7,11 +8,50 @@ def home(request):
     return render(request, 'payroll/index.html', {'menuLinks' : menuLinks,})
 
 def mainSite(request, param):
-    #folder = param
     menuLinks = ('customers', 'suppliers')
     customers = Customer.objects.all()
     suppliers = Supplier.objects.all()
+    if (param == 'newCustomer'):
+        number = request.POST['customerNumber']
+        name = request.POST['customerName']
+        addNewCustomer(name, number)
+        customers = Customer.objects.all()
+        return render(request, 'payroll/index.html', {'menuLinks' : menuLinks,
+                                                  'customers' : customers,
+                                                  'suppliers' : suppliers,
+                                                  'opt' : 'customers'})
+    if (param == 'newSupplier'):
+        number = request.POST['supplierNumber']
+        name = request.POST['supplierName']
+        addNewSupplier(name, number)
+        suppliers = Supplier.objects.all()
+        return render(request, 'payroll/index.html', {'menuLinks' : menuLinks,
+                                                  'customers' : customers,
+                                                  'suppliers' : suppliers,
+                                                  'opt' : 'suppliers'})
+    
     return render(request, 'payroll/index.html', {'menuLinks' : menuLinks,
                                                   'customers' : customers,
                                                   'suppliers' : suppliers,
                                                   'opt' : param})
+
+
+def addNewCustomer(name, number):
+    customer = Customer(number=number, name=name, date=timezone.now())
+    print 'Acction:'
+    print 'Adding customer to database:'
+    print 'customer number: ' + customer.number
+    print 'customer name: ' + customer.name
+    print 'register date: ' + str(customer.date)
+    customer.save()
+    
+def addNewSupplier(name, number):
+    supplier = Supplier(number=number, name=name, date=timezone.now())
+    print 'Acction:'
+    print 'Adding supplier to database:'
+    print 'supplier number: ' + supplier.number
+    print 'supplier name: ' + supplier.name
+    print 'register date: ' + str(supplier.date)
+    supplier.save()
+    
+    
